12aff2036d9c1ee1ef750d47dfbc63fdc978eac9
Lindholm <gustaf.lindholm@capgemini.com>
2019-10-16 10:16:35 +0200

    setting upp react app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "demo-note"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://vgjz5n9tah.execute-api.eu-central-1.amazonaws.com/prodL"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_cbz2qwd0E",
+      APP_CLIENT_ID: "pjm9ujvqbcgfulo0hpv13dvob",
+      IDENTITY_POOL_ID: "eu-central-1:2cb43f35-c0c1-4ccd-b269-0751da840a12"
+    }
+  };
+  
